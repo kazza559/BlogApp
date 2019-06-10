a5c074f2f63c6a9572791681849a8825ed05cb90
hungnd51 <hungnd51@fsoft.com.vn>
2019-06-10 12:41:54 +0700

    config router

--- src/App/App.js
+++ src/App/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import Home from "../Home/index";
 import Header from "../components/Header/Header";
 import Login from "../Login/Login";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/signin" component={Login} />
-        <Route path="/register" component={Login} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Login} />
+          <Route exact path="/register" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
