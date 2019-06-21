841296715518ad9d7b27e358c04c7545a0d99cc8
kazza559 <kazimhung559@gmail.com>
2019-06-21 07:39:32 +0700

    change direction header

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "../Home/index";
-import Header from "../components/Header/Header";
+import Header from "../components/header/Header";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import { Router, Route, Switch } from "react-router-dom";
 import { history } from "./../Helpers/history";
 import EditArticle from "../Editor/EditArticle";
-import Profile from '../Profile'
+import Profile from "../Profile";
 
 function App() {
   return (
