c0c094f5b247c9c00293a6027f6e58522de4052e
hungnd51 <hungnd51@fsoft.com.vn>
2019-07-03 11:03:36 +0700

    Updates

---
+++ precache-manifest.735732f8734f92b52c73512bcdada84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7241a27edcc12703d29cc7faacac887",
+    "url": "/BlogApp/index.html"
+  },
+  {
+    "revision": "b76591497e15fe9f3690",
+    "url": "/BlogApp/static/css/main.ed97d07f.chunk.css"
+  },
+  {
+    "revision": "a4ecd63c1195cf38f3f3",
+    "url": "/BlogApp/static/js/2.184bd462.chunk.js"
+  },
+  {
+    "revision": "b76591497e15fe9f3690",
+    "url": "/BlogApp/static/js/main.97982cab.chunk.js"
+  },
+  {
+    "revision": "bd9391930cffda8f0b99",
+    "url": "/BlogApp/static/js/runtime~main.ab26f838.js"
+  }
+]);
